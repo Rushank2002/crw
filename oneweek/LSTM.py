b5a043decd263e00bf6a898af0bb0738e77169fb
Rushank2002 <167817538+Rushank2002@users.noreply.github.com>
2024-05-19 04:43:58 +0530

    Update LSTM.py

--- oneweek/LSTM.py
+++ oneweek/LSTM.py
@@ -26,12 +26,12 @@ def close(data):
     data_scaled = scaler.fit_transform(data)
 
 # Create sequences for LSTM
-def create_sequences(data, seq_length):
-    X, y = [], []
-    for i in range(len(data) - seq_length):
-        X.append(data[i:i + seq_length])
-        y.append(data[i + seq_length])
-    return np.array(X), np.array(y)
+    def create_sequences(data, seq_length):
+        X, y = [], []
+        for i in range(len(data) - seq_length):
+            X.append(data[i:i + seq_length])
+            y.append(data[i + seq_length])
+        return np.array(X), np.array(y)
 
 seq_length = 7
 X, y = create_sequences(data_scaled, seq_length)
